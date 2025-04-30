bb636a7a2f3ee2b950d1ec206885e269f1eabb3b
Wermerson Macedo <wermersonm@gmail.com>
2025-04-30 20:31:19 -0300

    Estrutra condicional

---
+++ desenvolver-web-fullstack/modulo-javacript/aula-04/index.js
@@ -0,0 +1,9 @@
+let numero01 = Number(prompt("Numero 01"));
+
+if (numero01 > 5) {
+  console.log("é maior que 5");
+} else if (numero01 < 5) {
+  console.log("é menor que 5");
+} else {
+  console.log("é uma a 5");
+}
